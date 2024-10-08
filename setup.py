fadb6f5cae08ab8479a739ffb1d024985265ea3f
shaheennabi <ishaheennabi333@gmail.com>
2024-10-08 10:49:53 -0700

    setup file updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'Shaheen Nabi',
+    author_email= 'ishaheennabi333@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)

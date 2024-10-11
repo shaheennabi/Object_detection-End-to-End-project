fcc61c45aced6ddd7a3870b6137d7859529089b9
shaheennabi <ishaheennabi333@gmail.com>
2024-10-11 10:58:39 -0700

    updated a lot

--- SignLanguage/constant/training_pipeline/__init__.py
+++ SignLanguage/constant/training_pipeline/__init__.py
@@ -10,6 +10,5 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
-DATA_DOWNLOAD_URL: str = "https://github.com/entbappy/Branching-tutorial/raw/master/Sign_language_data.zip"
-
+DATA_DOWNLOAD_URL: str = "GDRIVE_CONNECTION"
 
--- SignLanguage/pipeline/training_pipeline.py
+++ SignLanguage/pipeline/training_pipeline.py
@@ -12,38 +12,38 @@ class TrainPipeline:
 
 
 
-def start_data_ingestion(self)-> DataIngestionArtifact:
-        try: 
-            logging.info(
-                "Entered the start_data_ingestion method of TrainPipeline class"
-            )
-            logging.info("Getting the data from URL")
+    def start_data_ingestion(self)-> DataIngestionArtifact:
+            try: 
+                logging.info(
+                    "Entered the start_data_ingestion method of TrainPipeline class"
+                )
+                logging.info("Getting the data from URL")
 
-            data_ingestion = DataIngestion(
-                data_ingestion_config =  self.data_ingestion_config
-            )
+                data_ingestion = DataIngestion(
+                    data_ingestion_config =  self.data_ingestion_config
+                )
 
-            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-            logging.info("Got the data from URL")
-            logging.info(
-                "Exited the start_data_ingestion method of TrainPipeline class"
-            )
+                data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+                logging.info("Got the data from URL")
+                logging.info(
+                    "Exited the start_data_ingestion method of TrainPipeline class"
+                )
 
-            return data_ingestion_artifact
+                return data_ingestion_artifact
 
-        except Exception as e:
-            raise SignException(e, sys)
-
-
+            except Exception as e:
+                raise SignException(e, sys)
 
 
 
 
-def run_pipeline(self) -> None:
-        try:
-            data_ingestion_artifact = self.start_data_ingestion()
-            
 
 
-        except Exception as e:
-            raise SignException(e, sys)
+    def run_pipeline(self) -> None:
+            try:
+                data_ingestion_artifact = self.start_data_ingestion()
+                
+
+
+            except Exception as e:
+                raise SignException(e, sys)

8f878338fa1e7bc56587976463f25caf654edc84
shaheennabi <ishaheennabi333@gmail.com>
2024-10-11 00:11:36 -0700

    constants and entities added

---
+++ SignLanguage/constant/training_pipeline/__init__.py
@@ -0,0 +1,20 @@
+import os 
+
+ARTIFACT_DIR: str = "artifacts"
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+
+DATA_DOWLOAD_URL: str = "https://drive.google.com/file/d/1FqRCsUcwN3Ryjh-yWHa8Y9qJwMse6u6J/view?usp=sharing"
+
+
+
+
+
+
+
---
+++ SignLanguage/entity/config_entity.py
@@ -0,0 +1,24 @@
+import os 
+from dataclasses import dataclass
+from datetime import datetime
+from SignLanguage.constant.training_pipeline import * 
+
+TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+
+
+@dataclass
+class TrainingPipelineConfig:
+    artifacts_dir: str = os.path.join(ARTIFACT_DIR, TIMESTAMP)
+
+
+
+training_pipeline_config: TrainingPipelineConfig = TrainingPipelineConfig()
+
+
+@dataclass
+class DataIngestionConfig: 
+    data_ingestion_dir: str = os.path.join(training_pipeline_config.artifacts_dir, DATA_INGESTION_DIR_NAME)
+
+    feature_store_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_FEATURE_STORE_DIR)
+
+    data_download_url: str = DATA_DOWLOAD_URL
